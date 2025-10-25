4bcb89e547023fc0a4181f604584cca50b64624b
jahseemm <jahseemullings@gmail.com>
2025-10-25 16:15:26 -0500

    new

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       {/* Background */}
-      <div className="top-left" style= {{top:'0', left:'0'}}>
+      <div className="top-left" style= {{top:"0", left:"0"}}>
         <div className="background">
           <img src="/imgs/bkg4.png" alt="Background" />
         </div>
@@ -20,49 +20,53 @@ function App() {
       <div className="bkgd-dark"></div>
 
       {/* Main Content */}
-      <div className="top-left" style={{top:'0', left:'0'}}>
+      <div className="top-left" style={{top:'-22px', left:'0'}}>
         <div className="flex-row-panel" style={{ gap: '60px' }}>
           {/* Left Panel: Welcome */}
-          <div className="panel" style={{ position: 'relative', backgroundColor: 'transparent', left: '0'}}>
-            <div className="normal-text">
-              <h1 style={{ color: 'aliceblue', fontSize: '40px' }}>Welcome to Doudle</h1>
-              <h2 style={{ color: 'aliceblue', marginTop: '-16px' }}>Let's finish TODO your Day</h2>
-              <p style={{ color: 'aliceblue', lineHeight: '1.5', fontWeight: 'lighter' }}>
-                A place to manage your tasks, from the simplest to the most complex.
-                <br /> All with A.I. assistance
-              </p>
-            </div>
+          <div className="panel" style={{ position: "relative", left: '0', backgroundColor: "transparent"}}>
+            <div className="top-left" style={{position:"relative", marginLeft:"1px", textAlign:"left"}}>
+              <div className="normal-text"> 
+                <h1 style={{ color: 'aliceblue', fontSize: '40px' }}>Welcome to Doudle</h1>
+                <h2 style={{ color: 'aliceblue', marginTop: '-16px' }}>Let's finish TODO your Day</h2>
+                <p style={{ color: 'aliceblue', lineHeight: '1.5', fontWeight: 'lighter', marginTop:"-10px" }}>
+                  A place to manage your tasks, from the simplest to the most complex.
+                  <br /> All with A.I. assistance
+                </p>
+              </div>
+            
+            
 
             {/* Social Media Icons */}
 
-            <div style={{ display: 'flex', gap: '10px', position: 'relative', marginTop: '-10px', }}>
-              <a href="https://www.facebook.com" className="button1" style={{ width: '26px', height: '26px', padding: '7px', backgroundColor: 'transparent' }}>
-                <img style={{ width: '40px' }} src="/imgs/icn/facebk.png" alt="Facebook" />
-              </a>
-              <a href="https://www.instagram.com" className="button1" style={{ width: '34px', height: '34px', padding: '3px', backgroundColor: 'transparent' }}>
-                <img style={{ width: '44px' }} src="/imgs/icn/insta.png" alt="Instagram" />
-              </a>
-              <a href="https://www.x.com" className="button1" style={{ width: '27px', height: '27px', padding: '7px', backgroundColor: 'transparent' }}>
-                <img style={{ width: '44px', borderRadius: '0' }} src="/imgs/icn/twitter.png" alt="X (Twitter)" />
-              </a>
-              <a href="https://www.youtube.com" className="button1" style={{ width: '34px', height: '34px', padding: '3px', backgroundColor: 'transparent' }}>
-                <img style={{ width: '44px' }} src="/imgs/icn/youtube.png" alt="YouTube" />
-              </a>
-            </div>
+              <div style={{ display: 'flex', gap: '10px', position: 'relative', marginTop: '-10px', marginLeft:"-10px" }}>
+                <a href="https://www.facebook.com" className="button1" style={{ width: '26px', height: '26px', padding: '7px', backgroundColor: 'transparent' }}>
+                  <img style={{ width: '40px' }} src="/imgs/icn/facebk.png" alt="Facebook" />
+                </a>
+                <a href="https://www.instagram.com" className="button1" style={{ width: '34px', height: '34px', padding: '3px', backgroundColor: 'transparent' }}>
+                  <img style={{ width: '44px' }} src="/imgs/icn/insta.png" alt="Instagram" />
+                </a>
+                <a href="https://www.x.com" className="button1" style={{ width: '27px', height: '27px', padding: '7px', backgroundColor: 'transparent' }}>
+                  <img style={{ width: '44px', borderRadius: '0' }} src="/imgs/icn/twitter.png" alt="X (Twitter)" />
+                </a>
+                <a href="https://www.youtube.com" className="button1" style={{ width: '34px', height: '34px', padding: '3px', backgroundColor: 'transparent' }}>
+                  <img style={{ width: '44px' }} src="/imgs/icn/youtube.png" alt="YouTube" />
+                </a>
+              </div>
 
-            
-            <p style={{ color: 'aliceblue', fontWeight: 'lighter', fontSize: '13px', marginTop: '1px' }}>
-              Follow us on Social Media
-            </p>
+              
+              <p style={{ color: 'aliceblue', fontWeight: 'lighter', fontSize: '13px', marginTop: '1px' }}>
+                Follow us on Social Media
+              </p>
+            </div>
           </div>
 
           {/* Right Panel: Sign-In */}
-          <div className="panel" style={{ position: 'relative', backgroundColor: 'transparent' }}>
+          <div className="panel" style={{ position: 'relative', backgroundColor: 'transparent', top:"20px"}}>
             <h1 className="center-text" style={{ marginTop: '10px', color: 'aliceblue', fontSize: '40px' }}>
               Sign-In
             </h1>
 
-            <form id="login-form" style={{ marginTop: '30px', display: 'flex', flexDirection: 'column', gap: '15px' }}>
+            <form id="login-form" style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', gap: '25px', textAlign:"left" }}>
               <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', gap: '5px' }}>
                 <label htmlFor="email" style={{ color: 'aliceblue', fontWeight: 'lighter' }}>
                   Email Address
@@ -89,15 +93,15 @@ function App() {
               </button>
             </div>
 
-            <p style={{ color: 'aliceblue', fontWeight: 'lighter', fontSize: '14px' }}>
-              Lost your password?{' '}
+            <p style={{ color: 'aliceblue', fontWeight: 'lighter', fontSize: '14px', marginTop:"20px" }}>
+              Lost your password?
               <a href="Pages/auth/lostpass.html" style={{ color: '#00bfff', textDecoration: 'none' }}>
                 Change Password
               </a>
             </p>
 
             <p style={{ color: 'aliceblue', fontWeight: 'lighter', fontSize: '14px', marginTop: '20px' }}>
-              No Account?{' '}
+              No Account?
               <a href="Pages/auth/sign-up.html" style={{ color: '#00bfff', textDecoration: 'none' }}>
                 Sign-Up
               </a>
